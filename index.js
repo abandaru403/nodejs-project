4be87cfed5bcf13ef515bf0b84e986f12926806b
abandaru403 <abandaru403@aol.com>
2025-06-22 16:44:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from Azure DevOps!'));
+app.get('/', (req, res) => res.send('Hello from NodeJs Azure DevOps!'));
 app.listen(port, () => console.log(`App listening on port ${port}`));
